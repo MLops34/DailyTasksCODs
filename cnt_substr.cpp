117beeb33177721151f0d45065acb6f77dd523d5
MLops34 <147312087+MLops34@users.noreply.github.com>
2024-08-03 12:41:24 +0530

    Update cnt_substr.cpp

--- cnt_substr.cpp
+++ cnt_substr.cpp
@@ -7,6 +7,7 @@
         for(int i=0;i<s.size();i++){
             map.clear();
             for(int j=i;j<s.size();j++){
+                map[s[j]]++;
                 if(map.size()==k){
                     cnt++;
                 }
