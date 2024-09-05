0b2935ce5b061ae742cc1de489413754d6a40650
MLops34 <147312087+MLops34@users.noreply.github.com>
2024-09-05 13:13:57 +0530

    Number os substring having distinct a,b,c

---
+++ TwoPointer/Numofsubs.cpp
@@ -0,0 +1,18 @@
+TwoPointer/Number of Substrings Containing All Three Characters.cpp
+
+    class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int hash[3]{-1,-1,-1};
+        int cnt=0;
+        for(int i=0;i<s.size();i++){
+            hash[s[i]-'a']=i;
+            if(hash[0]!=-1 && hash[1]!=-1 && hash[2]!=-1){
+                cnt += 1 + min(hash[0], min(hash[1], hash[2]));
+            }
+        }
+        return cnt;
+        
+    }
+};
+}
